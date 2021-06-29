43a5df2c75bf54c9763ddec5fb89d106b858c37d
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-06-29 12:02:28 +0800

    Formik Control Component

---
+++ src/components/FormikControl.js
@@ -0,0 +1,18 @@
+
+
+function FormikControl(props) {
+  const {control} = props
+
+  switch(control) {
+    case 'input':
+    case 'textarea':
+    case 'select':
+    case 'radio':
+    case 'checkbox':
+    case 'date':
+    default: return null
+    
+  }
+}
+
+export default FormikControl;
